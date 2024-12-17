8a8a0e5cff48349be147a231d4a8919a9fa44247
Milena <anticz.milena@gmail.com>
2024-12-17 08:25:12 -0500

    test

--- learning_react/src/App.jsx
+++ learning_react/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,6 @@ function App() {
 
   return (
     <>
-      
       <h1>Final App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
